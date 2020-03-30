7a7ae0eccd38fe228a07a4efbe8bd511a72ebbb5
Manoj.Mallidi <31582429+m84085821@users.noreply.github.com>
2020-03-29 23:34:02 -0700

    Update TransactionClient.java
    added client code

--- src/main/java/Client/TransactionClient.java
+++ src/main/java/Client/TransactionClient.java
@@ -1,4 +1,12 @@
 package Client;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Properties;
+
+import Util.*;
 
 /**
  * @author Krishna Chaitanya Pullela, Manoj Mallidi, Benoit Gallet
@@ -7,8 +15,45 @@ public class TransactionClient
 {
     public static void main(String[] args)
     {
+    	Util Util = new Util();
         //TODO In a for loop iterating over Util.nbTransactions, create at each iteration a Socket
         // Establish a connection with the server using the Socket (see Util class for the port)
         // Create a new TransactionServerProxy thread with the Socket connected to the server
+    	
+    	
+       
+    	for (int i = 0; i < Util.nbTransactions ; i++) {
+    		
+    		try {
+    			
+    			Properties prop = readPropertiesFile("TransactionServer.properties");
+    			
+    			Socket socketToServer = new Socket(prop.getProperty("SERVER_HOST"),Util.port);
+    			
+				new Thread(new TransactionServerProxy(socketToServer)).start();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
     }
+    
+    
+    public static Properties readPropertiesFile(String fileName) throws IOException {
+        FileInputStream fis = null;
+        Properties prop = null;
+        try {
+           fis = new FileInputStream(fileName);
+           prop = new Properties();
+           prop.load(fis);
+        } catch(FileNotFoundException fnfe) {
+           fnfe.printStackTrace();
+        } catch(IOException ioe) {
+           ioe.printStackTrace();
+        } finally {
+           fis.close();
+        }
+        return prop;
+     }
 }
