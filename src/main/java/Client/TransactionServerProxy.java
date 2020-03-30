fff3cc60b31dd2ff21d2a4a2c6f3a7eca89a3a4e
Manoj.Mallidi <31582429+m84085821@users.noreply.github.com>
2020-03-29 23:34:43 -0700

    Update TransactionServerProxy.java
    added proxy code

--- src/main/java/Client/TransactionServerProxy.java
+++ src/main/java/Client/TransactionServerProxy.java
@@ -1,6 +1,12 @@
 package Client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+
+import Util.Util;
 
 /**
  * @author Krishna Chaitanya Pullela, Manoj Mallidi, Benoit Gallet
@@ -9,15 +15,11 @@ public class TransactionServerProxy implements Runnable
 {
     private Socket socketToServer;
     private Thread serverProxyThread;
+    Util Util = new Util();
 
     public TransactionServerProxy(Socket socketToServer)
     {
-        //TODO Constructor
-    }
-
-    public void start()
-    {
-        //TODO Create the thread
+        this.socketToServer = socketToServer;
     }
 
     @Override
@@ -31,5 +33,24 @@ public class TransactionServerProxy implements Runnable
         // Include either the account number or the amount for the other steps of the transaction
         // Listen for the result of each request (e.g.: READ_RESULT or WRITE_RESULT)
         // When the transaction is done, leave
+    	
+    	try {
+			BufferedReader inputStream = new BufferedReader(new InputStreamReader(socketToServer.getInputStream()));
+			PrintWriter outputStream = new PrintWriter(socketToServer.getOutputStream(), true);
+			
+			int account1 = (int) Math.floor(Math.random() * Util.nbAccount);
+			int account2 = (int) Math.floor(Math.random() * Util.nbAccount);
+			int amount = (int) Math.ceil(Math.random() * Util.nbInitialAmount);
+			
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+    	
     }
 }
